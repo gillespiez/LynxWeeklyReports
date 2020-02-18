45a0c53b9895d9684e3c44820bb1556491bcb291
gillespie_z <170052@virtualwindow.co.za>
2020-02-18 13:48:01 +0200

    change background colour, and fix cards

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -5,13 +5,15 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatTableModule} from '@angular/material/table';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 const MaterialComponents= [
   MatButtonModule,
   MatSidenavModule,
   MatCardModule,
   MatToolbarModule,
-  MatTableModule
+  MatTableModule,
+  MatGridListModule
 ]
 
 @NgModule({
