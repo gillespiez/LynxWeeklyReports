d058c981386f18ed478d9c1891e7b9198bb6dfd8
gillespie_z <170052@virtualwindow.co.za>
2020-02-24 15:47:59 +0200

    add chevron to tables'

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -9,19 +9,20 @@ export interface VehicleInformation {
   l100: number;
   kml: number;
   odometer: number;
+  icon: string;
 }
 
 const ELEMENT_DATA: VehicleInformation[] = [
-  {vehicleID: "1", type: 'Demo', l100: 1.7, kml: 10, odometer: 12000},
-  {vehicleID: "2", type: 'Demo', l100: 4.6, kml: 8, odometer: 12000},
-  {vehicleID: "3", type: 'Rental', l100: 6.1, kml: 9,odometer: 12000},
-  {vehicleID: "4", type: 'Demo', l100: 9.2, kml: 4, odometer: 12000},
-  {vehicleID: "5", type: 'Rental', l100: 10.1, kml: 1, odometer: 12000},
-  {vehicleID: "6", type: 'Rental', l100: 12.7, kml: 1, odometer: 12000},
-  {vehicleID: "7", type: 'Rental', l100: 14.7, kml: 5, odometer: 12000},
-  {vehicleID: "8", type: 'Demo', l100: 15.4, kml:8, odometer: 12000},
-  {vehicleID: "9", type: 'Rental', l100: 18.9, kml: 5, odometer: 12000},
-  {vehicleID: "10", type: 'Demo', l100: 20.1, kml: 9, odometer: 12000},
+  {vehicleID: "1", type: 'Demo', l100: 1.7, kml: 10, odometer: 12000, icon:  'keyboard_arrow_right'},
+  {vehicleID: "2", type: 'Demo', l100: 4.6, kml: 8, odometer: 12000, icon:  'keyboard_arrow_right'},
+  {vehicleID: "3", type: 'Rental', l100: 6.1, kml: 9,odometer: 12000, icon:  'keyboard_arrow_right'},
+  {vehicleID: "4", type: 'Demo', l100: 9.2, kml: 4, odometer: 12000, icon:  'keyboard_arrow_right'},
+  {vehicleID: "5", type: 'Rental', l100: 10.1, kml: 1, odometer: 12000, icon:  'keyboard_arrow_right'},
+  {vehicleID: "6", type: 'Rental', l100: 12.7, kml: 1, odometer: 12000, icon:  'keyboard_arrow_right'},
+  {vehicleID: "7", type: 'Rental', l100: 14.7, kml: 5, odometer: 12000, icon:  'keyboard_arrow_right'},
+  {vehicleID: "8", type: 'Demo', l100: 15.4, kml:8, odometer: 12000, icon: 'keyboard_arrow_right'},
+  {vehicleID: "9", type: 'Rental', l100: 18.9, kml: 5, odometer: 12000, icon:  'keyboard_arrow_right'},
+  {vehicleID: "10", type: 'Demo', l100: 20.1, kml: 9, odometer: 12000, icon: 'keyboard_arrow_right'},
 ];
 interface Type {
   value: string;
@@ -40,7 +41,7 @@ export class DashboardComponent {
   ];
   title = 'material-demo';
   opened= 'opened';
-  displayedColumns: string[] = ['vehicleID', 'type', 'l100', 'kml', 'odometer'];
+  displayedColumns: string[] = ['vehicleID', 'type', 'l100', 'kml', 'odometer', 'icon'];
   dataSource = ELEMENT_DATA;
   maxDate = new Date();
   minDate = new Date(2017, 1, 1);
