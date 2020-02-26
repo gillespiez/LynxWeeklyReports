4ae61db6a164c3cad63fa7fc659db98a4bbb32dd
gillespie_z <170052@virtualwindow.co.za>
2020-02-26 13:21:32 +0200

    add interpolation for total vehicles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,34 +3,6 @@ import { MatIconRegistry } from "@angular/material/icon";
 import { DomSanitizer } from "@angular/platform-browser";
 import { ActivatedRoute } from '@angular/router';
 
-export interface Amount {
-  title: string,
-  total: number
-}
-
-export interface VehicleInformation {
-  vehicleID: string;
-  type: string;
-  l100: number;
-  kml: number;
-  odometer: number;
-}
-
-const AMOUNT_OF_VEHICLES: Amount[] = [
-  {title: "Total Cars", total: 42},
-];
-const ELEMENT_DATA: VehicleInformation[] = [
-  {vehicleID: "1", type: 'Demo', l100: 1.7, kml: 10, odometer: 12000},
-  {vehicleID: "2", type: 'Demo', l100: 4.6, kml: 8, odometer: 12000},
-  {vehicleID: "3", type: 'Rental', l100: 6.1, kml: 9,odometer: 12000},
-  {vehicleID: "4", type: 'Demo', l100: 9.2, kml: 4, odometer: 12000},
-  {vehicleID: "5", type: 'Rental', l100: 10.1, kml: 1, odometer: 12000},
-  {vehicleID: "6", type: 'Rental', l100: 12.7, kml: 1, odometer: 12000},
-  {vehicleID: "7", type: 'Rental', l100: 14.7, kml: 5, odometer: 12000},
-  {vehicleID: "8", type: 'Demo', l100: 15.4, kml:8, odometer: 12000},
-  {vehicleID: "9", type: 'Rental', l100: 18.9, kml: 5, odometer: 12000},
-  {vehicleID: "10", type: 'Demo', l100: 20.1, kml: 9, odometer: 12000},
-];
 interface Type {
   value: string;
   viewValue: string;
@@ -48,12 +20,13 @@ export class AppComponent {
   ];
 
   title = 'Babcock';
-  opened= 'opened'
-  displayedColumns: string[] = ['vehicleID', 'type', 'l100', 'kml', 'odometer'];
-  dataSource = ELEMENT_DATA;
-  amount = AMOUNT_OF_VEHICLES;
+  totalCars = 42;
+
+  opened= 'opened';
+
   maxDate = new Date();
   minDate = new Date(2017, 1, 1);
+
   constructor(
     private route: ActivatedRoute,
     private matIconRegistry: MatIconRegistry,
