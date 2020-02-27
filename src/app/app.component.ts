7827f315fa7b10bdd64b847f7eb9b0afa0704d1e
gillespie_z <170052@virtualwindow.co.za>
2020-02-27 11:45:36 +0200

    change quotation marks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { MatIconRegistry } from "@angular/material/icon";
-import { DomSanitizer } from "@angular/platform-browser";
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 
 interface Type {
@@ -36,77 +36,77 @@ export class AppComponent {
     private domSanitizer: DomSanitizer){
     this.matIconRegistry.addSvgIcon(
       `distance`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/distance.svg",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/distance.svg',
       )
     );
     this.matIconRegistry.addSvgIcon(
       `consumed`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/consumed.svg",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/consumed.svg',
       )
     );
     this.matIconRegistry.addSvgIcon(
       `speed`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/speed.svg",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/speed.svg',
       )
     );
     this.matIconRegistry.addSvgIcon(
       `time`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/time.svg",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/time.svg',
       )
     );
     this.matIconRegistry.addSvgIcon(
       `totalcars`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/totalcars.svg",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/totalcars.svg',
       )
     );
     this.matIconRegistry.addSvgIcon(
       `search`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/searchicon.svg",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/searchicon.svg',
       )
     );
     this.matIconRegistry.addSvgIcon(
       `calendar`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/calender.svg",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/calender.svg',
       )
     );
     this.matIconRegistry.addSvgIcon(
       `type`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/filtertype.svg",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/filtertype.svg',
       )
     );
     this.matIconRegistry.addSvgIcon(
       `dashAct`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/dashAct.svg",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/dashAct.svg',
       )
     );
     this.matIconRegistry.addSvgIcon(
       `demoAct`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/demoAct.svg",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/demoAct.svg',
       )
     );
     this.matIconRegistry.addSvgIcon(
       `rentalAct`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/rentalAct.svg",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/rentalAct.svg',
       )
     );
     this.matIconRegistry.addSvgIcon(
       `dash`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/dash.svg",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/dash.svg',
       )
     );
     this.matIconRegistry.addSvgIcon(
       `demos`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/demos.svg",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/demos.svg',
       )
     );
     this.matIconRegistry.addSvgIcon(
       `rentals`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/rentals.svg",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/rentals.svg',
       )
     );
     this.matIconRegistry.addSvgIcon(
       `sidenav`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/menubutt.svg",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/menubutt.svg',
       )
     );
   }
--- src/app/demos/demos.component.ts
+++ src/app/demos/demos.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { MatIconRegistry } from "@angular/material/icon";
-import { DomSanitizer } from "@angular/platform-browser";
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 
 export interface VehicleInformation {
@@ -13,16 +13,16 @@ export interface VehicleInformation {
 }
 
 const ELEMENT_DATA: VehicleInformation[] = [
-  {vehicleID: "DEM-0572 (FC57HBGP)", type: 'Demo', l100: 1.7, kml: 10, odometer: 12000, icon: 'keyboard_arrow_right'},
-  {vehicleID: "DEM-0572 (FC57HBGP)", type: 'Demo', l100: 4.6, kml: 8, odometer: 12000, icon: 'keyboard_arrow_right'},
-  {vehicleID: "DEM-0572 (FC57HBGP)", type: 'Demo', l100: 6.1, kml: 9,odometer: 1200, icon: 'keyboard_arrow_right'},
-  {vehicleID: "DEM-0572 (FC57HBGP)", type: 'Demo', l100: 9.2, kml: 4, odometer: 12000, icon: 'keyboard_arrow_right'},
-  {vehicleID: "DEM-0572 (FC57HBGP)", type: 'Demo', l100: 10.1, kml: 1, odometer: 12000, icon: 'keyboard_arrow_right'},
-  {vehicleID: "DEM-0572 (FC57HBGP)", type: 'Demo', l100: 12.7, kml: 1, odometer: 12000, icon: 'keyboard_arrow_right'},
-  {vehicleID: "DEM-0572 (FC57HBGP)", type: 'Demo', l100: 14.7, kml: 5, odometer: 12000, icon: 'keyboard_arrow_right'},
-  {vehicleID: "DEM-0572 (FC57HBGP)", type: 'Demo', l100: 15.4, kml:8, odometer: 12000, icon: 'keyboard_arrow_right'},
-  {vehicleID: "DEM-0572 (FC57HBGP)", type: 'Demo', l100: 18.9, kml: 5, odometer: 12000, icon: 'keyboard_arrow_right'},
-  {vehicleID: "DEM-0572 (FC57HBGP)", type: 'Demo', l100: 20.1, kml: 9, odometer: 12000, icon: 'keyboard_arrow_right'},
+  {vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 1.7, kml: 10, odometer: 12000, icon: 'keyboard_arrow_right'},
+  {vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 4.6, kml: 8, odometer: 12000, icon: 'keyboard_arrow_right'},
+  {vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 6.1, kml: 9,odometer: 1200, icon: 'keyboard_arrow_right'},
+  {vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 9.2, kml: 4, odometer: 12000, icon: 'keyboard_arrow_right'},
+  {vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 10.1, kml: 1, odometer: 12000, icon: 'keyboard_arrow_right'},
+  {vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 12.7, kml: 1, odometer: 12000, icon: 'keyboard_arrow_right'},
+  {vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 14.7, kml: 5, odometer: 12000, icon: 'keyboard_arrow_right'},
+  {vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 15.4, kml:8, odometer: 12000, icon: 'keyboard_arrow_right'},
+  {vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 18.9, kml: 5, odometer: 12000, icon: 'keyboard_arrow_right'},
+  {vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 20.1, kml: 9, odometer: 12000, icon: 'keyboard_arrow_right'},
 ];
 
 @Component({
@@ -52,77 +52,77 @@ export class DemosComponent {
     private domSanitizer: DomSanitizer){
     this.matIconRegistry.addSvgIcon(
       `distance`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/distance.svg",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/distance.svg',
       )
     );
     this.matIconRegistry.addSvgIcon(
       `consumed`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/consumed.svg",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/consumed.svg',
       )
     );
     this.matIconRegistry.addSvgIcon(
       `speed`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/speed.svg",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/speed.svg',
       )
     );
     this.matIconRegistry.addSvgIcon(
       `time`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/time.svg",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/time.svg',
       )
     );
     this.matIconRegistry.addSvgIcon(
       `totalcars`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/totalcars.svg",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/totalcars.svg',
       )
     );
     this.matIconRegistry.addSvgIcon(
       `search`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/searchicon.svg",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/searchicon.svg',
       )
     );
     this.matIconRegistry.addSvgIcon(
       `calendar`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/calender.svg",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/calender.svg',
       )
     );
     this.matIconRegistry.addSvgIcon(
       `type`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/filtertype.svg",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/filtertype.svg',
       )
     );
     this.matIconRegistry.addSvgIcon(
       `dashAct`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/dashAct.svg",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/dashAct.svg',
       )
     );
     this.matIconRegistry.addSvgIcon(
       `demoAct`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/demoAct.svg",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/demoAct.svg',
       )
     );
     this.matIconRegistry.addSvgIcon(
       `rentalAct`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/rentalAct.svg",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/rentalAct.svg',
       )
     );
     this.matIconRegistry.addSvgIcon(
       `dash`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/dash.svg",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/dash.svg',
       )
     );
     this.matIconRegistry.addSvgIcon(
       `demos`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/demos.svg",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/demos.svg',
       )
     );
     this.matIconRegistry.addSvgIcon(
       `rentals`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/rentals.svg",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/rentals.svg',
       )
     );
     this.matIconRegistry.addSvgIcon(
       `sidenav`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/menubutt.svg",
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/menubutt.svg',
       )
     );
   }
