5337d26d2d8f1365a4adbf5488b04f6dd38932d5
gillespie_z <170052@virtualwindow.co.za>
2020-03-04 08:58:36 +0200

    hardcode icon into table

--- src/app/modules/reports/dashboard/dashboard.component.ts
+++ src/app/modules/reports/dashboard/dashboard.component.ts
@@ -12,20 +12,19 @@ export interface VehicleInformation {
   l100: number;
   kml: number;
   odometer: number;
-  icon: string;
 }
 
 const ELEMENT_DATA: VehicleInformation[] = [
-  { vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 1.7, kml: 10, odometer: 12000, icon:  'keyboard_arrow_right' },
-  { vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 4.6, kml: 8, odometer: 12000, icon:  'keyboard_arrow_right' },
-  { vehicleID: 'REN-0238 (FYH887FS)', type: 'Rental', l100: 6.1, kml: 9, odometer: 12000, icon:  'keyboard_arrow_right' },
-  { vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 9.2, kml: 4, odometer: 12000, icon:  'keyboard_arrow_right' },
-  { vehicleID: 'REN-0238 (FYH887FS)', type: 'Rental', l100: 10.1, kml: 1, odometer: 12000, icon:  'keyboard_arrow_right' },
-  { vehicleID: 'REN-0238 (FYH887FS)', type: 'Rental', l100: 12.7, kml: 1, odometer: 12000, icon:  'keyboard_arrow_right' },
-  { vehicleID: 'REN-0238 (FYH887FS)', type: 'Rental', l100: 14.7, kml: 5, odometer: 12000, icon:  'keyboard_arrow_right' },
-  { vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 15.4, kml: 8, odometer: 12000, icon: 'keyboard_arrow_right' },
-  { vehicleID: 'REN-0238 (FYH887FS)', type: 'Rental', l100: 18.9, kml: 5, odometer: 12000, icon:  'keyboard_arrow_right' },
-  { vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 20.1, kml: 9, odometer: 12000, icon: 'keyboard_arrow_right' },
+  { vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 1.7, kml: 10, odometer: 12000 },
+  { vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 4.6, kml: 8, odometer: 12000 },
+  { vehicleID: 'REN-0238 (FYH887FS)', type: 'Rental', l100: 6.1, kml: 9, odometer: 12000 },
+  { vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 9.2, kml: 4, odometer: 12000 },
+  { vehicleID: 'REN-0238 (FYH887FS)', type: 'Rental', l100: 10.1, kml: 1, odometer: 12000 },
+  { vehicleID: 'REN-0238 (FYH887FS)', type: 'Rental', l100: 12.7, kml: 1, odometer: 12000 },
+  { vehicleID: 'REN-0238 (FYH887FS)', type: 'Rental', l100: 14.7, kml: 5, odometer: 12000 },
+  { vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 15.4, kml: 8, odometer: 12000 },
+  { vehicleID: 'REN-0238 (FYH887FS)', type: 'Rental', l100: 18.9, kml: 5, odometer: 12000 },
+  { vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 20.1, kml: 9, odometer: 12000 },
 ];
 
 @Component({
--- src/app/modules/reports/demos/demos.component.ts
+++ src/app/modules/reports/demos/demos.component.ts
@@ -9,20 +9,19 @@ export interface VehicleInformation {
   l100: number;
   kml: number;
   odometer: number;
-  icon: string;
 }
 
 const ELEMENT_DATA: VehicleInformation[] = [
-  {vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 1.7, kml: 10, odometer: 12000, icon: 'keyboard_arrow_right'},
-  {vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 4.6, kml: 8, odometer: 12000, icon: 'keyboard_arrow_right'},
-  {vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 6.1, kml: 9, odometer: 1200, icon: 'keyboard_arrow_right'},
-  {vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 9.2, kml: 4, odometer: 12000, icon: 'keyboard_arrow_right'},
-  {vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 10.1, kml: 1, odometer: 12000, icon: 'keyboard_arrow_right'},
-  {vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 12.7, kml: 1, odometer: 12000, icon: 'keyboard_arrow_right'},
-  {vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 14.7, kml: 5, odometer: 12000, icon: 'keyboard_arrow_right'},
-  {vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 15.4, kml: 8, odometer: 12000, icon: 'keyboard_arrow_right'},
-  {vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 18.9, kml: 5, odometer: 12000, icon: 'keyboard_arrow_right'},
-  {vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 20.1, kml: 9, odometer: 12000, icon: 'keyboard_arrow_right'},
+  {vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 1.7, kml: 10, odometer: 12000},
+  {vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 4.6, kml: 8, odometer: 12000},
+  {vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 6.1, kml: 9, odometer: 1200},
+  {vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 9.2, kml: 4, odometer: 12000},
+  {vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 10.1, kml: 1, odometer: 12000},
+  {vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 12.7, kml: 1, odometer: 12000},
+  {vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 14.7, kml: 5, odometer: 12000},
+  {vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 15.4, kml: 8, odometer: 12000},
+  {vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 18.9, kml: 5, odometer: 12000},
+  {vehicleID: 'DEM-0572 (FC57HBGP)', type: 'Demo', l100: 20.1, kml: 9, odometer: 12000},
 ];
 
 @Component({
